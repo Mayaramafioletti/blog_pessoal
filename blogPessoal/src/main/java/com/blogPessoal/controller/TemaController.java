e1289d48c92dae690d9fd3d17483b94e485459c9
Mayara Mafioletti <mayaracardinot@hotmail.com>
2021-03-03 20:33:17 -0300

    feat (controller): criação do findAll

--- blogPessoal/src/main/java/com/blogPessoal/controller/TemaController.java
+++ blogPessoal/src/main/java/com/blogPessoal/controller/TemaController.java
@@ -1,5 +1,28 @@
 package com.blogPessoal.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.blogPessoal.model.Tema;
+import com.blogPessoal.repository.TemaRepository;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping("/tema")
 public class TemaController {
+	
+	@Autowired
+	private TemaRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Tema>> getAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
 
 }
