fb1334f7b1778bbf88363bc4b203c3c993e6c815
Mayara Mafioletti <mayaracardinot@hotmail.com>
2021-03-03 20:34:59 -0300

    feat (controller): criação do findById

--- blogPessoal/src/main/java/com/blogPessoal/controller/TemaController.java
+++ blogPessoal/src/main/java/com/blogPessoal/controller/TemaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,13 @@ public class TemaController {
 	public ResponseEntity<List<Tema>> getAll(){
 		return ResponseEntity.ok(repository.findAll());
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Tema> getById(@PathVariable long id){
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	
 
 }
