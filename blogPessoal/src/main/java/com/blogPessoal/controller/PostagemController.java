abd4ba3edfe162cb6841cfe0a3b7478d46425186
Mayara Mafioletti <mayaracardinot@hotmail.com>
2021-03-03 16:18:35 -0300

    feat (controller): criação do controller de Postagem

---
+++ blogPessoal/src/main/java/com/blogPessoal/controller/PostagemController.java
@@ -0,0 +1,16 @@
+package com.blogPessoal.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.blogPessoal.repository.PostagemRepository;
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class PostagemController {
+	@Autowired
+	private PostagemRepository repository;
+}
