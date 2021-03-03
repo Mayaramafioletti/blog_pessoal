e8ff5715439d777f146594392927c71f9cf954ee
Mayara Mafioletti <mayaracardinot@hotmail.com>
2021-03-03 20:38:34 -0300

    feat (controller): criação do Put

--- blogPessoal/src/main/java/com/blogPessoal/controller/TemaController.java
+++ blogPessoal/src/main/java/com/blogPessoal/controller/TemaController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,5 +46,10 @@ public class TemaController {
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(repository.save(tema));
 	}
+	@PutMapping
+	public ResponseEntity<Tema> put (@RequestBody Tema tema){
+		return ResponseEntity.ok(repository.save(tema));				
+	}
+	
 
 }
