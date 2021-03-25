337c94e1ca1bdbd5ca15947d4f88a94a14b6154c
Mayara Mafioletti <mayaracardinot@hotmail.com>
2021-03-25 09:08:11 -0300

    fix(controller): adicionando getId no usuarioController

--- blogPessoal/src/main/java/com/blogPessoal/controller/UsuarioController.java
+++ blogPessoal/src/main/java/com/blogPessoal/controller/UsuarioController.java
@@ -6,19 +6,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.blogPessoal.model.UserLogin;
 import com.blogPessoal.model.Usuario;
+import com.blogPessoal.repository.UsuarioRepository;
 import com.blogPessoal.service.UsuarioService;
 
 @RestController
 @RequestMapping("/usuarios")
 @CrossOrigin("*")
 public class UsuarioController {
+	
+	@Autowired
+	private UsuarioRepository repository;
 	
 	
 	@Autowired
@@ -28,6 +35,11 @@ public class UsuarioController {
 	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user) {
 		return usuarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
 				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> GetById(@PathVariable long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
 	}
 
 	@PostMapping("/cadastrar")
@@ -39,6 +51,11 @@ public class UsuarioController {
 			return ResponseEntity.badRequest().build();
 		}
 		
+	}
+	
+	@PutMapping
+	public ResponseEntity<Usuario> Put(@RequestBody Usuario usuario) {
+		return ResponseEntity.ok(repository.save(usuario));
 	}
 
 
